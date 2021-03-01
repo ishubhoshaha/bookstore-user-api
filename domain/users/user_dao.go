5ad468248684480cea4adc69181cd250b7c88e73
Shubho Shaha <shubho.shaha@bkash.com>
2021-03-02 00:46:02 +0600

    insert data into table

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -5,6 +5,11 @@ import (
 	"github.com/ishubhoshaha/bookstore-user-api/mysql/users_db"
 	"github.com/ishubhoshaha/bookstore-user-api/utils/date_utils"
 	"github.com/ishubhoshaha/bookstore-user-api/utils/errors"
+	"strings"
+)
+
+const (
+	queryInsertUser = "INSERT INTO users(first_name, last_name, email, date_created) VALUES (?, ?, ?, ?);"
 )
 
 var (
@@ -31,15 +36,33 @@ func (user *User) Get() *errors.RestErr{
 }
 
 func (user *User) Save() *errors.RestErr {
-	current := userDB[user.Id]
-	if current != nil {
-		if current.Email == user.Email {
-			return errors.NewBadRequest(fmt.Sprintf("email %s already registered", user.Email))
-		}
-		return errors.NewBadRequest(fmt.Sprintf("user %d already exist", user.Id))
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
+	defer stmt.Close()
 	user.DateCreated = date_utils.GetNowString()
-	userDB[user.Id] = user
+	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+	if err != nil {
+		if strings.Contains(err.Error(), "email_UNIQUE") {
+			return errors.NewBadRequest(fmt.Sprintf("email %s already exists",user.Email))
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("Error when trying to saving user %s", err.Error()))
+	}
+	userId, err := insertResult.LastInsertId()
+	if err != nil {
+		return errors.NewInternalServerError(fmt.Sprintf("Error when trying to saving user %s", err.Error()))
+	}
+	//current := userDB[user.Id]
+	//if current != nil {
+	//	if current.Email == user.Email {
+	//		return errors.NewBadRequest(fmt.Sprintf("email %s already registered", user.Email))
+	//	}
+	//	return errors.NewBadRequest(fmt.Sprintf("user %d already exist", user.Id))
+	//}
+	//user.DateCreated = date_utils.GetNowString()
+	//userDB[user.Id] = user
+	user.Id = userId
 	return nil
 }
 
